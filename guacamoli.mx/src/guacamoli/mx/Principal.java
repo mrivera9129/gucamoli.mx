925551fd0c5248a7d3ecb018238a43d62bc21440
Ismael Natividad <isma3_l@hotmail.com>
2019-07-31 16:08:04 -0500

    agregue otra linea

--- guacamoli.mx/src/guacamoli/mx/Principal.java
+++ guacamoli.mx/src/guacamoli/mx/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("¡Bienvenido a Guacamoli!");
+		System.out.println("Hola a todos");
 	}
 
 }
